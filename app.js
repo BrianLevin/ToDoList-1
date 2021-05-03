12484f7d48d5cea0992181666f189fd89fd8be88
BrianLevin <bml201095@gmail.com>
2021-05-03 19:11:53 -0400

    used ejs to pass the kind of day it is in the condition

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const bodyParser = require("body-parser");
 // Creating app which utilizes the express package
 const app = express();
 
+// app will utilze ejs templating and view engine
+
+// the engine will look for files in the views folder by default
+app.set('view engine', 'ejs');
+
 // home route
 app.get("/", function (req, res) {
   // variable which  hold the new date meth
@@ -13,14 +18,23 @@ app.get("/", function (req, res) {
 
   //variable which will hold the get day method
   var currentDay = today.getDay();
+// hold empty day and will change and be used in ejs engine depending on condition
+  var day = "";
 
   // if statement to see if its the wekeend
   if (currentDay === 5 || currentDay === 0) {
-    res.write("<h1>It's the weekend!</h1>");
+      //weekend variable
+      day = "weekend";
+    
   } else {
-      // send over file in the directory when true
-    res.sendFile(__dirname  + "/index.html")
+
+    // weekday variable
+       day= "weekday";
+
+    
   }
+    // looks for file called list in views then pass variable to file
+    res.render("list", {kindOfDay:day})
 });
 
 // local server
